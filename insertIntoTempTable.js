f45b391012f7a591877e15b6c5a2a1611a9960e1
EricksonVaz <ericksoncv1@outlook.com>
2024-09-25 17:24:46 -0100

    feature insert into done

--- insertIntoTempTable.js
+++ insertIntoTempTable.js
@@ -15,42 +15,51 @@ const csuBdConnection = {
     requestTimeout:3600000
 }
 
-let arrListagemGeralPS = ListagemGeralPS[0].map(el=>`('${el.N}','${el.Nome}','${el.BI}','${el.Residencia}','${el.Concelho}')`);
+let arrListagemGeralPS = ListagemGeralPS[0].sort((a,b)=>(+a.N)-(+b.N)).map(el=>`('${escaparCaracter(el.N)}','${escaparCaracter(el.Nome)}','${escaparCaracter(el.BI)}','${escaparCaracter(el.Residencia)}','${escaparCaracter(el.Concelho)}')`);
 
-let arrListagemGeralPSPaginated = arrayPaginate(arrListagemGeralPS,1,1000);
+function escaparCaracter(val){
+    return (""+val??"").trim().replace(/'/g, "''");
+}
+
+
 
 async function execute(){
-    console.log("Executando save na base de dados")
-    let reult = await savePSFeedback(arrListagemGeralPSPaginated.docs)
-    console.log("Feito",reult)
+    // console.log("Executando save na base de dados")
+    // let reult = await savePSFeedback(arrListagemGeralPSPaginated.docs)
+    // console.log("Feito",reult)
+
+    //console.log(arrListagemGeralPSPaginated.docs.join(","))
+    let arrListagemGeralPSPaginated = arrayPaginate(arrListagemGeralPS,1,1000);
+
+
+    let {totalPages=1} = arrListagemGeralPSPaginated;
+
+    //console.log(totalPages,listToSave.length)
+    console.log("Strt loop")
+    let querySQL = ""
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        let newArrListagemGeralPS= [];
+        if(currentPage==1) newArrListagemGeralPS = arrListagemGeralPSPaginated.docs;
+        else newArrListagemGeralPS = arrayPaginate(arrListagemGeralPS,currentPage,1000).docs;
+
+
+        //console.log(currentPage,newArrListagemGeralPS.length)
+        querySQL = await savePSFeedback(newArrListagemGeralPS);
+
+    }
+
+    console.log("done")
 }
  execute();
 
 
-//console.log(arrListagemGeralPSPaginated.docs.join(","))
-            
 
-// let {totalPages=1} = arrListagemGeralPSPaginated;
-
-// console.log(totalPages,listToSave.length)
-
-// let querySQL = ""
-// for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-//     let newArrListagemGeralPS= [];
-//     if(currentPage==1) newArrListagemGeralPS = arrListagemGeralPSPaginated.docs;
-//     else newArrListagemGeralPS = arrayPaginate(arrListagemGeralPS,currentPage,1000).docs;
-
-
-//     console.log(currentPage,newArrListagemGeralPS.length)
-//     querySQL = await savePSFeedback(newArrListagemGeralPS);
-
-// }
 
 async function savePSFeedback(valuesToSave){
     let pool = await sqlServer.connect(csuBdConnection);
 
     let querySQL = `
-        INSERT INTO [CSU_APIs_Feedback_Loops].[dbo].[CSU_PS_Julho_202]
+        INSERT INTO [CSU_APIs_Feedback_Loops].[dbo].[CSU_PS_Julho_2024]
             ([N]
             ,[Nome]
             ,[BI]
